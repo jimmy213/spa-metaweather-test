52d791602e6b1b8592c58133689dd0459c0720d1
jimmy213 <jimaster213@gmail.com>
2021-05-21 18:20:41 +0300

    Fetched data selection

--- src/components/SubView.js
+++ src/components/SubView.js
@@ -6,10 +6,18 @@ const SubView = ({ weather }) => {
                     <ul className="weather-data">
                         <li>
                             <h4>{ weather.applicable_date }</h4>
-                            <h2>{ weather.weather_state_name }</h2>
-                            <h3>{ weather.max_temp }</h3>
-                            <h3>{ weather.min_temp }</h3>
-                            <h3>{ weather.humidity }</h3>
+                            <div className="condition">
+                                <img src={`https://www.metaweather.com/static/img/weather/${weather.weather_state_abbr}.svg`} alt="Icon" />
+                                <h2>{ weather.weather_state_name }</h2>
+                            </div>
+                            <div className="temps">
+                                <h3>Current: { Math.trunc(weather.the_temp) }°C</h3>
+                                <h3>Max: { Math.trunc(weather.max_temp) }°C</h3>
+                                <h3>Min: { Math.trunc(weather.min_temp) }°C</h3>
+                            </div>
+                            <h3>Humidity: { weather.humidity }%</h3>
+                            <h3>Visibility: { weather.visibility.toFixed(1) } miles</h3>
+                            <h3>Pressure: { weather.air_pressure }mb</h3>
                         </li>
                     </ul>
                 </div>
