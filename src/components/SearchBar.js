57be8ef775d4e0d151a2be4077c8ffc3ef897b39
jimmy213 <jimaster213@gmail.com>
2021-05-21 17:21:13 +0300

    Check input type

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,19 +7,27 @@ const SearchBar = () => {
     const [citySearch, setCitySearch] = useState(null);
 
     const handleClick = inp => {
-        fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${inp}`)
-        .then(res => res.json())
-        .then(data => {
-            setCitySearch(data);
-        });
+        if (/\d/.test(inp)) {
+            fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?lattlong=${inp}`)
+            .then(res => res.json())
+            .then(data => {
+                setCitySearch(data);
+            });
+        } else {
+            fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${inp}`)
+            .then(res => res.json())
+            .then(data => {
+                setCitySearch(data);
+            });
+        }
     }
 
     return (
         <div className="search-bar">
-            <h2>Enter City name or Coordinates "e.g. 36.96,-122.02"</h2>
+            <h2>Enter City name or Coordinates (comma separated)</h2>
             <div className="search-term">
-                <input type="text" placeholder="Search by City..." value={input} onInput={e => setInput(e.target.value)} />
-                <button onClick={ () => handleClick(input) }>Submit</button>
+                <input type="text" placeholder="e.g. Tokyo, 36.96,-122.02" value={input} onInput={e => setInput(e.target.value)} />
+                <button onClick={ () => handleClick(input) }>Search</button>
             </div>
             {citySearch && <MainView results={citySearch} />}
         </div>
